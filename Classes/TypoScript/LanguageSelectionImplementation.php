43851b2121aa5a5e03de907e98abd75bf1c3ea0c
Dominique Feyer <dfeyer@ttree.ch>
2016-10-05 20:08:33 +0200

    TASK: Basic WebStarter:Header.LanguageSelection
    See: https://swiss.github.io/styleguide/en/header.html#02-header-05-language-selector

---
+++ Classes/TypoScript/LanguageSelectionImplementation.php
@@ -0,0 +1,29 @@
+<?php
+namespace Ttree\SwissConfederation\WebStarter\TypoScript;
+
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Neos\TypoScript\DimensionsMenuImplementation;
+use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
+use TYPO3\TypoScript\Exception as TypoScriptException;
+
+/**
+ * TypoScript implementation for a language selection menu.
+ */
+class LanguageSelectionImplementation extends DimensionsMenuImplementation
+{
+    /**
+     * @param string $pinnedDimensionName
+     * @param NodeInterface $nodeInDimensions
+     * @param array $targetDimensions
+     * @return string
+     */
+    protected function itemLabel(string $pinnedDimensionName, NodeInterface $nodeInDimensions, array $targetDimensions): string
+    {
+        if ($pinnedDimensionName === null) {
+            return $nodeInDimensions->getLabel();
+        }
+        $label = $targetDimensions[$pinnedDimensionName]['value'];
+        return strtoupper($label);
+    }
+
+}
